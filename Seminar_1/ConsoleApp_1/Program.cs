38f72909b608173ae2429d1d3af7f6d478eae72a
Dulebova Emma <emma2105@yandex.ru>
2022-10-21 11:21:34 +0300

    add ConsoleApp 1

---
+++ Seminar_1/ConsoleApp_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа, которая на вход принимает целое число и выводит его квадрат
+
+// Запрос числа от пользователя
+Console.Write("Введите цtлое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// Квадрат исходного кода
+int sqrNumber = number * number;
+
+// Вывод результата на экран
+Console.WriteLine($"Квадрат вашего числа = {sqrNumber}");
