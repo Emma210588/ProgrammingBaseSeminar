8aa531902d45cf703e7fb08ab26b264a9376a952
Dulebova Emma <emma2105@yandex.ru>
2022-10-21 11:57:17 +0300

    add ConsoleApp 2

---
+++ Seminar_1/ConsoleApp_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа, которая на вход принимает два числа и поверяет, является ли первое число квадратом второго
+Console.Write("Введите первое целое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите первое целое число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 == number2 * number2)
+{
+   Console.Write("Первое число является квадратом второго");  
+}
+else
+{
+    Console.Write ("Первое число не является квадратом второго"); 
+}
