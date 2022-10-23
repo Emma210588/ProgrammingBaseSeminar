cf2d6f410ba6877d846700a800b6c8aaf52258d0
Dulebova Emma <emma2105@yandex.ru>
2022-10-23 21:00:41 +0300

    Add CommonConsoleApp2

---
+++ Seminar_2/CommonConsoleApp2/CommonConsoleApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+456-> 46
+782-> 72
+918-> 98*/
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+
+int last = number % 10;
+int first = number / 100;
+int result = first * 10 + last;
+
+Console.WriteLine($"Случайное число без второй цифры равно: {result}");
