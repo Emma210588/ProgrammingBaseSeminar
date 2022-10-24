89d9d8b7040dbc7723d525582bccee5a4ee1a98b
Dulebova Emma <emma2105@yandex.ru>
2022-10-24 07:36:05 +0300

    add ConsoleAppHW2_2

---
+++ Homework_2/ConsoleAppHW2_2/ConsoleAppHW2_2/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Напишите программу, которая выводит третью цифру заданного числа
+или сообщает, что третьей цифры нет.
+645-> 5
+78->третьей цифры нет
+32679 -> 6 */
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100)
+    Console.Write("Третьей цифры нет");
+else
+{
+    while (number > 999)
+    {
+       number = number / 10;
+    }
+
+    int result = number % 10;
+    Console.Write($"Третья цифра заданного числа: {result}");
+}
+
