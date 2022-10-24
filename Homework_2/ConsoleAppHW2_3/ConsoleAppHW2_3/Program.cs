9c870ed3c32bd39b7881ef3df1a92e9204c65059
Dulebova Emma <emma2105@yandex.ru>
2022-10-24 07:45:53 +0300

    add ConsoleAppHW2_3_

---
+++ Homework_2/ConsoleAppHW2_3/ConsoleAppHW2_3/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+и проверяет, является ли этот день выходным.
+6->да
+7->да
+1->нет*/
+
+Console.Write("Введите число дня недели: ");
+int numberWeek = Convert.ToInt32(Console.ReadLine());
+
+if (numberWeek > 7 || numberWeek < 1)
+{
+    Console.Write("Дней недели всего 7");
+}
+
+else
+{
+    if (numberWeek == 6 || numberWeek == 7) 
+    {
+        Console.Write("Да");
+    } 
+        
+    else
+    {
+        Console.Write("Нет");
+    }
+       
+}
