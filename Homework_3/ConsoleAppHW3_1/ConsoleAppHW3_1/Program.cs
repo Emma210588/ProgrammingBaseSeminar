bae32a1df3af2beebfba91e17b61e68a15b3be97
Dulebova Emma <emma2105@yandex.ru>
2022-10-28 20:32:05 +0300

    Add ConsoleAppHW3_1

---
+++ Homework_3/ConsoleAppHW3_1/ConsoleAppHW3_1/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Напишите программу, которая принимает на вход пятизначное число и проверяет,
+является ли оно палиндромом.
+14212->нет
+12821->да
+23432->да*/
+
+using System;
+
+int reverseDigits(int number)
+{
+    int revNum = 0;
+    while (number > 0)
+    {
+        revNum = revNum * 10 + number % 10;
+        number = number / 10;
+    }
+    return revNum;
+}
+
+Console.WriteLine("Введи пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 9999 && number < 100000)
+{
+int revNum = reverseDigits(number);
+    if (number == revNum) Console.WriteLine($"{number} - Палиндром");
+    else Console.WriteLine($"{number} - Не палиндром");
+}
+else
+{
+    Console.WriteLine($"ОШИБКА: {number} - не является пятизначным");
+}
