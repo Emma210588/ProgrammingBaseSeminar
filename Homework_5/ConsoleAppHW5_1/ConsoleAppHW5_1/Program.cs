f6b51d8c1c3b4929a801d811c296aa2b0386984e
Dulebova Emma <emma2105@yandex.ru>
2022-11-13 07:46:41 +0300

    ConsoleAppHW5_1

---
+++ Homework_5/ConsoleAppHW5_1/ConsoleAppHW5_1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int QuantityEvenInteger(int[] array) 
+{
+    int count = 0;
+    for (int i = 0; i < 15; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;  
+
+}
+
+int[] array = new int[15];
+for (int i = 0; i < 15; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+    Console.WriteLine(array[i] + " ");
+}
+ 
+Console.WriteLine(QuantityEvenInteger(array));
+ 
