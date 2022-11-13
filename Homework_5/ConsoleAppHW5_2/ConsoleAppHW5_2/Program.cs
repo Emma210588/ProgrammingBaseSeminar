b23dc297d3db735b34c90dec14fe42c0bc0448a7
Dulebova Emma <emma2105@yandex.ru>
2022-11-13 08:11:47 +0300

    ConsoleAppHW5_2

---
+++ Homework_5/ConsoleAppHW5_2/ConsoleAppHW5_2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6]-> 0
+
+int sumUnevenNumbers(int[] array) 
+{
+    int Sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 1)
+        {
+            Sum += array[i];
+        }
+    }
+    
+
+    return Sum;
+}
+int[] array = new int[5];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1,10);
+    Console.WriteLine(array[i] + " ");
+}
+ 
+Console.WriteLine($"Cумма элементов, стоящих на нечётных позициях равна {sumUnevenNumbers(array)}");
