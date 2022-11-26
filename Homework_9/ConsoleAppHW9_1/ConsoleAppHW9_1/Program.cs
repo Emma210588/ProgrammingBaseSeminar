cc21ac069bebfea98093a05dd31dd840f82eaaa7
Dulebova Emma <emma2105@yandex.ru>
2022-11-26 08:30:18 +0300

    ConsoleAppHW9_1

---
+++ Homework_9/ConsoleAppHW9_1/ConsoleAppHW9_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии.
+// N = 5-> "5, 4, 3, 2, 1"
+// N = 8-> "8, 7, 6, 5, 4, 3, 2, 1"
+
+void NaturalNum(int n)
+{
+    Console.Write(n + " ");
+    if (n == 1)
+    {
+        return;
+    }
+    NaturalNum(n - 1);
+}
+
+Console.WriteLine("Vvedite N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+NaturalNum(n);
